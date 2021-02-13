c2503a6e6690711843490315369cf6d34a38206e
Carlos <carlosaugustocf99@gmail.com>
2021-02-12 21:49:20 -0300

    Create component ButtonReady

---
+++ src/Components/ButtonReady/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Container, ContainerButtonReady} from './styles'
+
+
+const ButtonReady: React.FC = () => {
+  return (
+    <Container>
+      <ContainerButtonReady />
+    </Container>
+    
+  );
+}
+
+export default ButtonReady;
---
+++ src/Components/ButtonReady/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 180px;
+    outline: none;
+`
+
+
+export const ContainerButtonReady = styled.button`
+    outline: none;
+    cursor: pointer;
+    border-radius: 5px;
+    border: none;
+    width: 400px;
+    height: 140px;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
+`
